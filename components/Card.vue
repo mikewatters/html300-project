58a359718e63a811e47eb316f422349ddb1994bd
Mike Watters <mxmikewatters@gmail.com>
2020-04-17 15:07:18 -0700

    Merge?

--- components/Card.vue
+++ components/Card.vue
@@ -15,16 +15,12 @@ export default {
 <template>
 <!-- Cat image cards -->
 <!-- Cat image cards -->
-<div class="card">
-<b-card-img-lazy :src="card.url"></b-card-img-lazy>
+<div class="card bg-light text-white">
+  <img :src="card.url" class="card-img" :alt="card.alt">
 </div>
 
 </template>
 
-<style>
-.red-border {
-    border: 3px solid #6c757d;
-    box-shadow: 3px 3px 6px #6c757d;
+<style scoped>
 
-}
 </style>
--- pages/dogs/index.vue
+++ pages/dogs/index.vue
@@ -19,7 +19,7 @@ export default {
     mounted(){
       axios.defaults.headers.common['x-api-key'] = "151c477d-020a-437e-9f7b-b7d06c7eff38"
       axios
-        .get('https://api.thedogapi.com/v1/images/search', { params: { limit:33, size:"thumb" } } )
+        .get('https://api.thedogapi.com/v1/images/search', { params: { limit:9, size:"thumb" } } )
         .then(response => {
           this.cards = response.data
         })
@@ -37,10 +37,7 @@ export default {
   <h1>These are a few of my favorite dogs</h1>
   <!-- Image content goes here -->
   <p>I'm calling The Dog API for nine incredible dogs!</p>
-      <b-button v-if="myVal" @click="myMethod()" class="btn-success" v-b-popover.hover.top="'Make these cats go hmmm.'" title="Do it now">
-      Click me to put a border on the dogs
-    </b-button>
-    <p>&nbsp;</p>
+
   <section class="card-columns" v-if="cards">
     <card v-for="card in cards" :img="card.url" :card="card" :key="card.id">
     </card>
